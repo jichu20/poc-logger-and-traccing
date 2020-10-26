09c53b7ec087c947d4edc27cab4a013fa143fc5a
Borja Javier Sánchez Yuste <borjajavier.sanchez.next@bbva.com>
2020-10-26 12:33:42 +0100

    refactor ç

--- src/main/java/com/poc/logger/PocLoggerApplication.java
+++ src/main/java/com/poc/logger/PocLoggerApplication.java
@@ -1,15 +1,10 @@
 package com.poc.logger;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PocLoggerApplication {
-
-	@Autowired
-	private static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocLoggerApplication.class, args);
